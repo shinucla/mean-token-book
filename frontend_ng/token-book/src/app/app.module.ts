0429ee0f74ee478d4b55f4aecbcc9b7df7601082
shin@mac <shin@mac>
2019-04-16 21:42:03 -0700

    adding components and service placeholders

--- frontend_ng/token-book/src/app/app.module.ts
+++ frontend_ng/token-book/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeChildComponent } from './components/home-child/home-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    HomeChildComponent
   ],
   imports: [
     BrowserModule,
